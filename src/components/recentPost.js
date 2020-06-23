23d4a2018411ee53d1f07a191fdea4a3ddc4cf0f
rmyric <rmyric1@gmail.com>
2020-06-23 18:36:54 -0400

    Recent posts

--- src/components/recentPost.js
+++ src/components/recentPost.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+
+import * as actions from '../actions';
+
+
+
 class RecentPosts extends Component {
+    componentDidMount() {
+        this.props.fetchRecentPosts();
+    }
+    
     render() {
         return (
             <div classNamr="recent-posts">
@@ -10,7 +20,6 @@ class RecentPosts extends Component {
                             <li>recent post 0</li>
                             <li>recent post 1</li>
                             <li>recent post 2</li>
-                            <li>recent post 3</li>
                         </ul>
 
                         </div>
@@ -21,4 +30,4 @@ class RecentPosts extends Component {
     }
 }
 
-export default RecentPosts;
+export default connect(null, actions)(RecentPosts);
