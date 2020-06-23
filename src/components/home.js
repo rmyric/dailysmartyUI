eb87c7cc9da7a7b63c86b84ba64db9db9f098212
rmyric <rmyric1@gmail.com>
2020-06-23 17:09:29 -0400

    routes

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,7 @@ import Logo from './logo';
 import SearchBar from './searchbar';
 import RecentPosts from './recentPost'
 
-export default class App extends Component {
+export default class Home extends Component {
   render() {
     return (
       <div>
